c1d3526b1fc9404d5f611ba149df57acdc385421
LeoAntunesBrombilla <antunes.b.leonardo@gmail.com>
2022-01-08 13:57:59 -0300

    adding linting and prettier

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,8 +1,8 @@
-import request from "supertest";
-import app from "../index";
+import request from 'supertest';
+import app from '../index';
 
-describe("GET /", () => {
-  it("responds with Hello World!", (done) => {
-    request(app).get("/").expect("Hello World!", done);
+describe('GET /', () => {
+  it('responds with Hello World!', (done) => {
+    request(app).get('/').expect('Hello World!', done);
   });
 });
