585784236e14151ed076c29a758619b3811e6870
LeoAntunesBrombilla <antunes.b.leonardo@gmail.com>
2022-01-08 15:07:20 -0300

    testing husky

--- src/test/test.ts
+++ src/test/test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import app from '../index';
 
 describe('GET /', () => {
-  it('responds with Hello World!', (done) => {
-    request(app).get('/').expect('Hello World!', done);
+  it('responds with Hello World!!', (done) => {
+    request(app).get('/').expect('Hello World!!', done);
   });
 });
