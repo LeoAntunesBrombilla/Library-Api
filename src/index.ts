3208e49ee7fa2c1abc1169ff075169b1f95931f9
LeoAntunesBrombilla <antunes.b.leonardo@gmail.com>
2022-01-08 21:20:36 -0300

    fixing deploy

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 
-export const app = express();
+const app = express();
+const PORT = process.env.PORT || 2999;
 
 app.get('/', (req, res) => {
   res.send('Hello World!!');
+});
+
+app.listen(PORT, () => {
+  console.log('Our app is working!');
 });
 
 export default app;
--- src/server.ts
+++
@@ -1,4 +0,0 @@
-import app from '.';
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT);
